867ee8a953a3847c6db4ed32d9c0039e10285e68
samuelcmax <samuelswetyzera@gmail.com>
2025-06-09 14:09:20 -0300

    New Alert Event

---
+++ src/main/java/ac/grim/grimac/api/event/events/GrimAlertEvent.java
@@ -0,0 +1,44 @@
+package ac.grim.grimac.api.event.events;
+
+import ac.grim.grimac.api.AbstractCheck;
+import ac.grim.grimac.api.GrimUser;
+import ac.grim.grimac.api.event.Cancellable;
+import ac.grim.grimac.api.event.GrimEvent;
+import lombok.Getter;
+
+public class GrimAlertEvent extends GrimEvent implements Cancellable {
+
+    @Getter
+    private final GrimUser user;
+    @Getter
+    private final AbstractCheck check;
+    @Getter
+    private final String details;
+    @Getter
+    private final boolean verbose;
+
+    private boolean cancelled;
+
+    public GrimAlertEvent(GrimUser user, AbstractCheck check, String details, boolean verbose) {
+        this.user = user;
+        this.check = check;
+        this.details = details;
+        this.verbose = verbose;
+    }
+
+    @Override
+    public boolean isCancelled() {
+        return cancelled;
+    }
+
+    @Override
+    public void setCancelled(boolean cancelled) {
+        this.cancelled = cancelled;
+    }
+
+    @Override
+    public boolean isCancellable() {
+        return true;
+    }
+
+}
