1e744f0abb2d4d516bb524120000d78eba8198a4
samuelcmax <samuelswetyzera@gmail.com>
2025-06-09 15:54:12 -0300

    offset

--- src/main/java/ac/grim/grimac/api/event/events/AlertEvent.java
+++ src/main/java/ac/grim/grimac/api/event/events/AlertEvent.java
@@ -6,7 +6,7 @@ import ac.grim.grimac.api.event.Cancellable;
 import ac.grim.grimac.api.event.GrimEvent;
 import lombok.Getter;
 
-public class AlertEvent extends GrimEvent implements GrimUserEvent, Cancellable {
+public class AlertEvent extends CompletePredictionEvent implements GrimUserEvent, Cancellable {
 
     private final GrimUser user;
 
@@ -14,13 +14,14 @@ public class AlertEvent extends GrimEvent implements GrimUserEvent, Cancellable 
     private final AbstractCheck check;
     @Getter
     private final String details;
+
     @Getter
     private final boolean verbose;
 
     private boolean cancelled;
 
-    public AlertEvent(GrimUser user, AbstractCheck check, String details, boolean verbose) {
-        super(true);
+    public AlertEvent(GrimUser user, AbstractCheck check, double offset, String details, boolean verbose) {
+        super(user, check, offset);
         this.user = user;
         this.check = check;
         this.details = details;
